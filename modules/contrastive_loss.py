02687a60bfa460aa83ae52a9b36fc6a56ba627d3
Piotr Przemielewski <piotrusieg@Piotrs-MacBook-Air.local>
2023-01-23 19:01:40 +0100

    add simclr loss to contrastive loss

--- modules/contrastive_loss.py
+++ modules/contrastive_loss.py
@@ -74,12 +74,21 @@ class ClusterLoss(nn.Module):
         ne_j = math.log(p_j.size(0)) + (p_j * torch.log(p_j)).sum()
         ne_loss = ne_i + ne_j
 
-        c = self.bn(c_i).T @ self.bn(c_j)
-        # sum the cross-correlation matrix between all gpus
-        c.div_(self.batch_size)
+        c_i = c_i.t()
+        c_j = c_j.t()
+        N = 2 * self.class_num
+        c = torch.cat((c_i, c_j), dim=0)
+        sim = self.similarity_f(c.unsqueeze(1), c.unsqueeze(0)) / self.temperature
+        sim_i_j = torch.diag(sim, self.class_num)
+        sim_j_i = torch.diag(sim, -self.class_num)
 
-        on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
-        off_diag = off_diagonal(c).pow_(2).sum()
-        loss = on_diag + 0.0051 * off_diag
+
+        positive_clusters = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(N, 1)
+        negative_clusters = sim[self.mask].reshape(N, -1)
+
+        labels = torch.zeros(N).to(positive_clusters.device).long()
+        logits = torch.cat((positive_clusters, negative_clusters), dim=1)
+        loss = self.criterion(logits, labels)
+        loss /= N
 
         return loss + ne_loss
